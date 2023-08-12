2f509e9389a556bc383958228c8580df87e004f0
Lorain-stack <lakalbona807@gmail.com>
2023-08-12 07:12:59 -0700

    program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Description: This program will assign a random number to the variable
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
