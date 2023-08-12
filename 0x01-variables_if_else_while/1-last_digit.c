00aec1362f7c4f5cf3ff29ec3e8042249baa603c
Lorain-stack <lakalbona807@gmail.com>
2023-08-12 08:24:05 -0700

    o positive negative zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,6 +16,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	p = n % 10;
 	if (p > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, p);
+	if (p == 5)
 		printf("Last digit of %d is %d and is 0\n", n, p);
 	if (p < 6 && p != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
