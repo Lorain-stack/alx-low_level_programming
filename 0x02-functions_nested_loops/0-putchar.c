1b228f79177f1c5c3005e013f0cf381937b5a027
Lorain-stack <lakalbona807@gmail.com>
2023-08-15 15:45:30 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*main - Entry point for programme
+*This function demonstrates the usage of the putchar function.
+*It prints characters to the standard output.
+*Return: On success 1
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
