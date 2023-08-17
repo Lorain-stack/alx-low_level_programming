a2b4338f6a93e48daf2519671f80470a01de230b
Lorain-stack <lakalbona807@gmail.com>
2023-08-17 08:43:14 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - A program that prints numbers from 0 to 9, new line
+*Description: print number 0 through 9
+*Return: 0 on success
+*/
+
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 48; m <= 58; m++)
+	{
+		_putchar(m);
+	}
+		_putchar('\n');
+}
